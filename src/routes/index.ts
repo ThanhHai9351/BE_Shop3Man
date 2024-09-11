4cfee8d585cd30b3a1e01e7186c08f564a6b0be2
ThanhHai9351 <haihailua123456@gmail.com>
2024-09-11 16:32:29 +0700

    feat: send mailer

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Application } from "express";
 import UserRouter from "./user-router"; 
 import CategoryRouter from './category-router';
 import ImageRouter from './image-router'
+import MailerRouter from './mailer-router'
 
 const routes = (app: Application): void => {
   app.use("/api/user", UserRouter);
   app.use("/api/category", CategoryRouter);
   app.use("/api/image",ImageRouter);
+  app.use("/api/mail",MailerRouter);
 };
 
 export default routes;
---
+++ src/routes/mailer-router.ts
@@ -0,0 +1,9 @@
+import express, { Router } from "express";
+import MailerController from "../controllers/mailer-controller";
+
+const router: Router = express.Router();
+
+router.post("/sendMail", MailerController.sendMail);
+
+
+export default router;
