a84047d2ac894d3d1f79466a41c69d0f45279f08
ThanhHai9351 <haihailua123456@gmail.com>
2024-11-13 00:38:51 +0700

    initial socket.io

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,24 @@ import bodyParser from "body-parser"
 import cors from "cors"
 import { createClient } from "redis"
 import routes from "./routes"
+import { createServer } from "http"
+import { Server } from "socket.io"
 dotenv.config()
 
 const app: Application = express()
 const port: string | number = process.env.PORT || 3001
+const portSocket: number = 3002;
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173", 
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+  },
+  transports: ["websocket", "polling"],  
+  connectionStateRecovery: {}
+});
 
 const redisClient = createClient({
   password: process.env.REDIS_PW,
@@ -47,8 +61,10 @@ app.get("/api/status", (req: Request, res: Response) => {
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // Allow frontend origin
-    credentials: true, // Allow cookies to be sent
+    origin: "http://localhost:3000", 
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+    credentials: true, 
   }),
 )
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -58,3 +74,21 @@ routes(app)
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 })
+
+io.on('connection', (socket) => {
+  console.log('Socket connection established');
+  socket.broadcast.emit('hi');
+
+  socket.on('chat message', (msg: string) => {
+    console.log(msg);
+    io.emit('chat message', msg); 
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(portSocket, () => {
+  console.log(`Socket is running on http://localhost:${portSocket}`);
+});
